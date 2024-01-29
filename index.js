ed56f343fc1560890ab8049d8ab73d1aea19b632
mahisurrahman <mahisur.rahman.001@gmail.com>
2024-01-29 11:54:10 +0600

    JWT Implementation Done

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ async function run() {
       }
     });
 
-    //Posting Users//
+    // // Posting Users//
     app.post("/users", async (req, res) => {
       try {
         const user = req.body;
@@ -59,6 +59,7 @@ async function run() {
         console.log(err);
       }
     });
+
 
     //Posting Contests//
     app.post("/contests", async (req, res) => {
@@ -80,6 +81,23 @@ async function run() {
         console.log(err);
       }
     });
+
+
+    //Posting JWT Token//
+    app.post('/jwt', async(req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res.
+      cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV ==='production',
+        sameSite: process.env.NODE_ENV ==='production', 'none': 'strict',
+      })
+      .send({success: true})
+    })
+
 
     //Putting Users to DB//
     // app.put('/users/:email', async(req, res)=>{
